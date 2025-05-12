40f7b35703473069408944918c82c5bc774f5413
sidhanta33crs <sidhanta.rout@33crores.com>
2025-05-12 18:51:15 +0530

    commit

--- android/app/src/main/java/com/tms/MainActivity.kt
+++ android/app/src/main/java/com/tms/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.shreejagannathadham
+package com.shreejagannatha.dham
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/tms/MainApplication.kt
+++ android/app/src/main/java/com/tms/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.shreejagannathadham
+package com.shreejagannatha.dham
 
 import android.app.Application
 import com.facebook.react.PackageList
